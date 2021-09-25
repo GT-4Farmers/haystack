462377415f948663f75e5878ba16a067ab30a373
soobak <jnam08@gmail.com>
2021-09-24 20:04:20 -0400

    Fix merge conflicts manually

--- backend/routes/aboutRoute.js
+++ backend/routes/aboutRoute.js
@@ -5,6 +5,6 @@ editAboutRoute = require("../controllers/editAboutController");
 
 router.get("/", aboutRoute.getAboutController);
 router.put("/", editAboutRoute.editAboutController);
-router.put("/:bio", editAboutRoute.editBioController);
+// router.put("/:bio", editAboutRoute.editBioController);
 
 module.exports = router;
